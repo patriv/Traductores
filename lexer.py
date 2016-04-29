1de4c383fefb401bec03674395a6273871e6593c
Cinthya Ramos <09-11237@usb.ve>
2016-04-30 04:27:40 +0800

    ya casi listo :) solo falata mejorar la expresion regular de los Id, ya que tomo not como un Id

--- lexer.py
+++ lexer.py
@@ -42,16 +42,14 @@ reserved = {
 	#Boolenos
 	'true'  : 'TkTrue',
 	'false' : 'TkFalse',
-	 #Es reservada, no?
 
 	#Tipos 
 	'char'  : 'TkChar',
-	'bool'  : 'TkBool', #es bool??
+	'bool'  : 'TkBool', 
 	'int'   : 'TkInt',
 	'matrix': 'TkMatrix',
 
 	'of'    : 'TkOf'
-
 }
 
 # Tokens relevantes
@@ -90,7 +88,7 @@ tokens = [
 	'TkMenor',
 	'TkMenorIgual',
 	'TkMayor',
-	'TkMayoIgual',
+	'TkMayorIgual',
 	'TkIgual',
 	'TkSiguienteCar',
 	'TkAnteriorCar',
@@ -104,17 +102,20 @@ tokens = [
 
 #Ignora Tabulador y espacio uno o mas veces
 t_ignore = ' \t'
-t_TkNum = '\d+'
 
 #Ignoramos comentarios simples
 t_ignore_commentSimple = r'\%\%(.)*'
 
 #Ignoramos los comentarios de secciones completas
-t_ignore_comment = r'\%{([^-]|([-]+[^\$]))*}%'
-    
+def t_ignore_comment(t):
+	r'\%{([^-]|([-]+[^\$]))*\}%'
+	t.lexer.lineno += t.value.count('\n') 
 
+#Definimos la expresion para los digitos decimales
+t_TkNum = '\d+'
+    
 #Definimos las expresiones para los separadores
-t_TkComa = r','  #PREGUNTAR
+t_TkComa = r','
 t_TkPunto = r'\.'
 t_TkDosPuntos = r':'
 t_TkParAbre = r'\('
@@ -132,39 +133,35 @@ t_TkResta = r'-'
 t_TkMult = r'\*'
 t_TkDiv = r'/'
 t_TkMod = r'%'
-t_TkConjuncion = r'/\\' #no se
-t_TkDisyuncion = r'\\/' #no se
+t_TkConjuncion = r'\/\\'
+t_TkDisyuncion = r'\\/'
 t_TkNegacion = r'not'
 t_TkMenor = r'<'
 t_TkMenorIgual = r'<='
 t_TkMayor = r'>'
-t_TkMayoIgual = r'>='
+t_TkMayorIgual = r'>='
 t_TkIgual = r'='
 t_TkSiguienteCar = r'\+\+'
-t_TkAnteriorCar = r'\-\-'  #no se!!
-t_TkValorAscii = r'[#]' #???
+t_TkAnteriorCar = r'\-\-' 
+t_TkValorAscii = r'[#]' 
 t_TkConcatenacion = r'::'
 t_TkRotacion = r'\$'
-t_TkTrasposicion = r'\?' #??
+t_TkTrasposicion = r'\?'
 
-'''
-#Definimos literales numericos
-def TkNum (t):
-	r'\d+'
-	lexer_tokenList.append(t.lexer)
-	'''
 
 #Expresion regular para TkId
 def t_TkId(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    r'[a-zA-Z][a-zA-Z_0-9]*'
     t.type = reserved.get(t.value,'TkId')    # Check for reserved words
     return t
 
 
 #Expresion regular para un caracter
 
-
-
+def t_TkCaracter(t):
+	r'\'((\\t)|(\\n)|([^\']))\''
+	t.value = str(t.value)
+	return t
 
 #Cuenta numero de linea
 def t_newline(t):
@@ -172,16 +169,17 @@ def t_newline(t):
     t.lexer.lineno += len(t.value)
 
 #Cuenta numero de columna 
-def findColumn (t):
-	last_cr = lexer.lexdata.rfind('\n',0,t.lexpos)
+def find_column(t):
+	last_cr = lexer.lexdata.rfind('\n', 0, t.lexpos)
 	if last_cr < 0:
-		last_cr = 0
-	column = t.lexpos - last_cr
+		column = t.lexpos + 1
+	else:
+		column = t.lexpos - last_cr
 	return column
 
 #Errores de caracteres no esperados
 def t_error (t):
-	errorCaracter = 'ERROR: Caracter inesperado "{0}" en la fila {1}, columna {2}' .format(t.value[0], t.lineno, findColumn(t))
+	errorCaracter = 'ERROR: Caracter inesperado "{0}" en la fila {1}, columna {2}' .format(t.value[0], t.lineno, find_column(t))
 	lexer_error.append(errorCaracter)
 	t.lexer.skip(1)
 
@@ -196,59 +194,49 @@ lexer_error = []
 #Creamos una lista de nuestros tokens
 lexer_tokenList = []
 
-#Construccion del lexer
+# #Construccion del lexer
 lexer = lex.lex()
 
-# Test it out
-data = '''
 
-%{ hol }%}%
-}%
-with
-	var contador: int
-3 + 4 * 10
-  + -20 *2 
- end
-'''
-z = lexer.input(data)
+def analyzeNeo (code) :
+
+	lexer.input(code)
 
 
-while True:
-    tok = lexer.token()
-    if not tok: 
-        break  
-    lexer_tokenList.append(tok)    
-    
-   
-if len(lexer_error) != 0:
-    print_error()
-else: 
-	for t in lexer_tokenList:
-    
-        	print t
+	while True:
+	    tok = lexer.token()
+	    if not tok: 
+	        break  
+	    lexer_tokenList.append(tok)    
+	    
+	   
+	if len(lexer_error) != 0:
+	    print_error()
+	else: 
+		print('\n'+"----------------------------BIENVENIDO----------------------------"+'\n')
+		print('\n'+"Los tokens reconocidos en el archivo son: "+'\n')
+		nlinea = -1
+		tok = []
+		for t in lexer_tokenList:
+			if (not(t.lineno == nlinea)) and not(t==lexer_tokenList[0]):
+				tok.append('\n')
+
+			nlinea = t.lineno
+			if t.type == 'TkId':
+				tok.append("{0}(\"{1}\") {2} {3}".format(t.type,t.value,t.lineno,find_column(t)))
+			elif (t.type == 'TkNum') or (t.type == 'TkCaracter'):
+				tok.append("{0}({1}) {2} {3}".format(t.type,t.value,t.lineno,find_column(t)))
+			else:
+				tok.append("{0} {1} {2}".format(t.type,t.lineno,find_column(t)))
+			tok.append(', ')
+
+		toke= "".join(tok)
+
+		print toke[:(len(toke)-2)]+'\n'
+		
 
 lex.lex()
-'''
-#Construimos el lexer
-def build_lexer(code):
-    lexer = lex.lex()
-    lexer.input(code)
 
-    while True:
-    	tok = lexer.token()
-    	if not tok: 
-       		break 
-    	lexer_tokenList.append(tok)    
 
-    if lexer_error != []:
-    	print_error()
-    else:
-    	for t in lexer_tokenList:
-    
-        	print t
-    print("no sirve")
-'''
 if __name__ == '__main__':
 	pass
-	
-#	t_newline(t)
