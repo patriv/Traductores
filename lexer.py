e6de01d7182ce562b2beda929ac53311a8f8762a
Cinthya Ramos <09-11237@usb.ve>
2016-05-05 14:06:17 +0800

    ya casi listo :)

--- lexer.py
+++ lexer.py
@@ -19,7 +19,7 @@ reserved = {
 
 	'begin' : 'TkBegin',
 	'end'   : 'TkEnd',
-	'whith' : 'TkWith',
+	'with' : 'TkWith',
 	'var'   : 'TkVar',
 
 	#Condicional
@@ -45,10 +45,11 @@ reserved = {
 
 	#Tipos 
 	'char'  : 'TkChar',
-	'bool'  : 'TkBool', #es bool??
+	'bool'  : 'TkBool', 
 	'int'   : 'TkInt',
 	'matrix': 'TkMatrix',
 
+	'of'    : 'TkOf'
 }
 
 # Tokens relevantes
@@ -57,7 +58,7 @@ tokens = [
 	'TkId',
 
 	#Literales numericos
-	'TkNum'
+	'TkNum',
 
 	#Literales Caracter
 	'TkCaracter',
@@ -87,7 +88,7 @@ tokens = [
 	'TkMenor',
 	'TkMenorIgual',
 	'TkMayor',
-	'TkMayoIgual',
+	'TkMayorIgual',
 	'TkIgual',
 	'TkSiguienteCar',
 	'TkAnteriorCar',
@@ -98,99 +99,144 @@ tokens = [
 
 ] + list(reserved.values())
 
-#Ignora cualquier tipo de espacio
-#Tabulador y espacio uno o mas veces
+
+#Ignora Tabulador y espacio uno o mas veces
 t_ignore = ' \t'
 
-#Ignoramos los comentarios
-t_ignore_comments = r'%{.*}%'
+#Ignoramos comentarios simples
+t_ignore_commentSimple = r'\%\%(.)*'
 
-# Reglas de expresiones regulares para tokens simples
-t_TkComa = r'\,'
+#Ignoramos los comentarios de secciones completas
+def t_ignore_comment(t):
+	r'\%{([^-]|([-]+[^\$]))*\}%'
+	t.lexer.lineno += t.value.count('\n') 
+
+#Definimos la expresion para los digitos decimales
+t_TkNum = '\d+'
+    
+#Definimos las expresiones para los separadores
+t_TkComa = r','
 t_TkPunto = r'\.'
-t_TkDosPuntos = r'\:'
+t_TkDosPuntos = r':'
 t_TkParAbre = r'\('
 t_TkParCierra = r'\)'
 t_TkCorcheteAbre = r'\['
 t_TkCorcheteCierra = r'\]'
-t_TkLlaveAbre = r'\{'
-t_TkLlaveCierra = r'\}'
-t_TkHacer = r'\->'
-t_TkAsignacion = r'\<-'
+t_TkLlaveAbre = r'{'
+t_TkLlaveCierra = r'}'
+t_TkHacer = r'\->' #no se   
+t_TkAsignacion = r'\<-' #no se
+
+#Definimos las expresiones para operadores
 t_TkSuma = r'\+'
-t_TkResta = r'\-'
+t_TkResta = r'-'
 t_TkMult = r'\*'
-t_TkDiv = r'\/'
-t_TkMod = r'\%'
-t_TkConjuncion = r'\/\\' #REVISAR
-t_TkDisyuncion = r'\\\/' #REVISAR
-t_TkNegacion = r'\not'
-t_TkMenor = r'\<'
+t_TkDiv = r'/'
+t_TkMod = r'%'
+t_TkConjuncion = r'\/\\'
+t_TkDisyuncion = r'\\/'
+t_TkNegacion = r'not'
+t_TkMenor = r'<'
 t_TkMenorIgual = r'<='
-t_TkMayor = r'\>'
+t_TkMayor = r'>'
 t_TkMayorIgual = r'>='
-t_TkIgual = r'\='
-t_TkSiguienteCar = r'\++'
-t_TkAnteriorCar = r'\-' #ES IGUAL A LA RESTA? deberia ser --
-t_TkValorAscii = r'\#'
-t_TkConcatenacion = r'\::'
+t_TkIgual = r'='
+t_TkSiguienteCar = r'\+\+'
+t_TkAnteriorCar = r'\-\-' 
+t_TkValorAscii = r'[#]' 
+t_TkConcatenacion = r'::'
 t_TkRotacion = r'\$'
 t_TkTrasposicion = r'\?'
 
+
+#Expresion regular para TkId
+def t_TkId(t):
+    r'[a-zA-Z][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'TkId')    # Check for reserved words
+    return t
+
+
+#Expresion regular para un caracter
+
+def t_TkCaracter(t):
+	r'\'((\\t)|(\\n)|([^\']))\''
+	t.value = str(t.value)
+	return t
+
+#Cuenta numero de linea
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-def find_column(code,t):
-    last_cr = code.rfind('\n',0,t.lexer.lexpos)
-    if last_cr < 0:
-        last_cr = 0
-    
-    column = (t.lexpos - last_cr)
-    return column    
+#Cuenta numero de columna 
+def find_column(t):
+	last_cr = lexer.lexdata.rfind('\n', 0, t.lexpos)
+	if last_cr < 0:
+		column = t.lexpos + 1
+	else:
+		column = t.lexpos - last_cr
+	return column
 
-#Retorna un string conteniendo la linea y la columna
-def getLineAndColumn(code,t):
-    return '(Línea {0}, Columna {1})' .format(t.lexer.lineno, find_column(code,t))
+#Errores de caracteres no esperados
+def t_error (t):
+	errorCaracter = 'ERROR: Caracter inesperado "{0}" en la fila {1}, columna {2}' .format(t.value[0], t.lineno, find_column(t))
+	lexer_error.append(errorCaracter)
+	t.lexer.skip(1)
 
-#Manejo de errores  
-
-def t_error(t):
-    errorString  = 'Error: se encontro un caracter inesperado "{0}"' .format(t.value[0])
-    errorString +=  getLineAndColumn(t.lexer.lexdata,t)
-    lexer_errorList.append(errorString)
-    t.lexer.skip(1)
+def print_error():
+    print('\n'.join(lexer_error))
 
 
-#Lista de errores del Lexer
-lexer_errorList = []
 
-#Lista de tokens del Lexer
+#Creamos Lista de errores
+lexer_error = []
+
+#Creamos una lista de nuestros tokens
 lexer_tokenList = []
 
-#Construccion del lexer
+# #Construccion del lexer
 lexer = lex.lex()
 
-def build_lexer(code):
-    lexer = lex.lex()
-    lexer.input(code)
 
-    # Analiza los tokens
-    while True:
-       	tok = lexer.token()
+def analyzeNeo (code) :
 
-        if not tok:
-            break
+	lexer.input(code)
 
-        lexer_tokenList.append(tok)
 
-    # Imprime resultados del analisis realizado
-        if len(self._errorlist) != 0:
-        	for er in lexer_errorList:
-        		print er
-        else:
-            for tok in lexer_tokenList:
-        		print tok
+	while True:
+	    tok = lexer.token()
+	    if not tok: 
+	        break  
+	    lexer_tokenList.append(tok)    
+	    
+	   
+	if len(lexer_error) != 0:
+	    print_error()
+	else: 
+		print('\n'+"----------------------------BIENVENIDO----------------------------"+'\n')
+		print('\n'+"Los tokens reconocidos en el archivo son: "+'\n')
+		nlinea = -1
+		tok = []
+		for t in lexer_tokenList:
+			if (not(t.lineno == nlinea)) and not(t==lexer_tokenList[0]):
+				tok.append('\n')
+
+			nlinea = t.lineno
+			if t.type == 'TkId':
+				tok.append("{0}(\"{1}\") {2} {3}".format(t.type,t.value,t.lineno,find_column(t)))
+			elif (t.type == 'TkNum') or (t.type == 'TkCaracter'):
+				tok.append("{0}({1}) {2} {3}".format(t.type,t.value,t.lineno,find_column(t)))
+			else:
+				tok.append("{0} {1} {2}".format(t.type,t.lineno,find_column(t)))
+			tok.append(', ')
+
+		toke= "".join(tok)
+
+		print toke[:(len(toke)-2)]+'\n'
+		
+
+lex.lex()
+
 
 if __name__ == '__main__':
 	pass
